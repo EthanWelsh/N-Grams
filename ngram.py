adf537749799f37911f2a0d3bcc99616b89f3e4f
Ethan Welsh <welshej@gmail.com>
2016-02-09 11:28:28 -0500

    tokenization and sentence segmenting

--- ngram.py
+++ ngram.py
@@ -1,4 +1,4 @@
-import operator
+from nltk.tokenize import sent_tokenize
 
 
 class NGram:
@@ -35,11 +35,16 @@ class NGram:
         return max(iter(options) if options else iter(prefix_dict), key=prefix_dict.get)
 
 
+def get_sentences(untokenized_text):
+    untokenized_text = untokenized_text.lower()
+    return [('<s0> <s1> ' + sentence + ' </s>').split() for sentence in sent_tokenize(untokenized_text)]
+
+
 def main():
     bigram = NGram(2)
-    bigram.train([['<s>', 'the', 'cat', 'jumped', 'on', 'the', 'cat', '</s>'],
-                  ['<s>', 'the', 'cat', 'dumped', 'on', 'the', 'cat', '</s>']])
 
+    sentences = get_sentences('the cat jumped on the other cat. How was your day? My name is cat, what is yours Mr. Crouch?')
+    bigram.train(sentences)
     print(bigram.get_most_likely(('the',)))
 
 if __name__ == '__main__':
