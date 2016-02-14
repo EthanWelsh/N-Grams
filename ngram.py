dc77f6f8e8f1157718dd92f81c7026ca448ad519
Ethan Welsh <welshej@gmail.com>
2016-02-14 15:52:08 -0500

    handle unknown words in input

--- ngram.py
+++ ngram.py
@@ -59,11 +59,16 @@ class NGram:
 
     def successor_probabilities(self, prefix):
 
-        prefix = [word if word in self.unigrams else 'UNK' for word in prefix]
-        unigrams = dict([(word, self.unigrams[word] / self.word_count) for word in self.unigrams.keys()])
-        bigrams = dict(
-            [(word, self.ngrams[prefix[-2:]].get_probability(word)) for word in self.ngrams[prefix[-2:]].keys()])
-        trigrams = dict([(word, self.ngrams[prefix].get_probability(word)) for word in self.ngrams[prefix].keys()])
+        prefix = tuple(word if word in self.unigrams else '<UNK>' for word in prefix)
+
+        unigrams = dict([(word, self.unigrams[word] / self.word_count)
+                         for word in self.unigrams.keys()])
+
+        bigrams = dict([(word, self.ngrams[prefix[-2:]].get_probability(word))
+                        for word in self.ngrams[prefix[-2:]].keys()]) if prefix[-2:] in self.ngrams else {}
+
+        trigrams = dict([(word, self.ngrams[prefix].get_probability(word))
+                         for word in self.ngrams[prefix].keys()]) if prefix in self.ngrams else {}
 
         word_probabilities = {word: (.1 * unigrams.get(word, 0) +
                                      .4 * bigrams.get(word, 0) +
@@ -139,7 +144,7 @@ def train_corpus(directory_path, clean_routine=None):
 
 
 def main():
-    ngram = train_corpus('training_data', remove_gutenberg_disclamer)
+    ngram = train_corpus('dev_data', remove_gutenberg_disclamer)
 
     while True:
         print('>', end=' ')
